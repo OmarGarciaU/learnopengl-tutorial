598297ef0c80dd81b700d3cabb0050a6bc220064
Omar Garcia <gomar0428@gmail.com>
2026-07-13 21:27:41 -0700

    Added a moving triangle

--- shaders/src/main.cpp
+++ shaders/src/main.cpp
@@ -1,9 +1,11 @@
 #include "include/glad/glad.h"
 #include "include/GLFW/glfw3.h"
-#include "vec3.h"
 #include "shaders/shader.h"
-#include <cstdlib>
+#include "vec3.h"
+#include <cmath>
+#include <ctime>
 #include <iostream>
+
 
 using namespace std;
 
@@ -13,122 +15,121 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-/*
- * Vertex Shader
-*/
-
 int main() {
-
-  glfwInit();
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
-
-  // glfw window creation
-  // --------------------
-  GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "EnigmaTek", NULL, NULL);
-
-  //Winodw creation failure
-  //---------------------
-  if (window == NULL) {
-    std::cout << "Failed to create GLFW window" << std::endl;
-    glfwTerminate();
-    return -1;
-  }
-
-  glfwMakeContextCurrent(window);
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-  // glad: load all OpenGL function pointers
-  // ---------------------------------------
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-    std::cout << "Failed to initialize GLAD" << std::endl;
-    return -1;
-  }
-
-  // Build And Compile shader program
-  // ------------------------------- 
-  const char vertPath[] = "/Users/omargarcia/Desktop/Programming/Repos/Learning_and_Books/learnopengl-tutorial/shaders/src/shaders/shader.vs";
-  const char fragPath[] = "/Users/omargarcia/Desktop/Programming/Repos/Learning_and_Books/learnopengl-tutorial/shaders/src/shaders/shader.fs";
-  Shader Shader(vertPath,fragPath);
-  // set up vertex data (and buffer(s)) and configure vertex attributes
-  // ------------------------------------------------------------------
-  float vertices[]{
-     0.5f,  -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
-    -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-     0.0f,   0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
-  };
-  
-  unsigned int VAO, VBO;
-  glGenVertexArrays(1, &VAO);
-  glGenBuffers(1,&VBO);
-  // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-  glBindVertexArray(VAO);
-
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-  glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,6*sizeof(float), (void*)0);
-  glEnableVertexAttribArray(0);
-
-  glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE, 6*sizeof(float),(void*)(3*sizeof(float)));
-  glEnableVertexAttribArray(1);
-
-  //glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
-
-  //Random generator
-  srand(time(0));
-
-  //Generate a random color
-  color color(rand() % 255 / 255.0,rand() % 255 / 255.0,rand() % 255 / 255.0);
-
-  double a = 1.0f;
-
-  color.print_rgb();
-
-  // render loop
-  // -----------
-  while (!glfwWindowShouldClose(window)) {
-    // input
-    processInput(window);
-
-    // render 
-    glClearColor(color.x(), color.y(), color.z(), a);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-
-    Shader.use();
-    Shader.setFloat("someUniform", 1.0f);
-    glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-
-    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved
-    // etc.)
-    // -------------------------------------------------------------------------------
     
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL", NULL, NULL);
 
-  glDeleteVertexArrays(1,&VAO);
-  glDeleteBuffers(1, &VBO);
+    if(window == NULL){
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
 
-  // glfw: terminate, clearing all previously allocated GLFW resources.
-  // ------------------------------------------------------------------
-  glfwTerminate();
-  return 0;
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    //glad: load all openGL function pointers
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        cout << "Failed to initialize GLAD" << endl;
+        return -1;
+    }
+
+    const char vertPath[] = "/Users/omargarcia/Desktop/Programming/Repos/Learning_and_Books/learnopengl-tutorial/shaders/src/shaders/shader.vs";
+    const char fragPath[] = "/Users/omargarcia/Desktop/Programming/Repos/Learning_and_Books/learnopengl-tutorial/shaders/src/shaders/shader.fs";
+
+    Shader Shader(vertPath, fragPath);
+
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f, // Bottom left
+         0.5f, -0.5f, 0.0f, // Bottom Right
+         0.0f,  0.5f, 0.0f  // Top
+    };
+
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // 3*sizeof(float) the stride i.e how many elements in each row
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
+
+
+    //Random generator
+    srand(time(0));
+
+    color color(0.5f, 0.2f, 0.9f);
+
+    double a = 1.0f;
+
+    color.print_rgb();
+
+    int click = 0;
+
+    while(!glfwWindowShouldClose(window)){
+        //input
+        processInput(window);
+
+        glClearColor(color.x(), color.y(), color.z(), a);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //render
+
+        Shader.use();
+        glBindVertexArray(VAO);
+        //update every thousand clicks
+        if(click < 15){
+            click++;
+        }else{
+            for(int i = 0; i< 9; i+=3){
+                vertices[i] = vertices[i] + 0.05f;
+                if(vertices[i] > 1.0) vertices[i] = -1.0f;
+            }
+            click = 0;
+        }
+
+        cout << click;
+        
+
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // 3*sizeof(float) the stride i.e how many elements in each row
+        glEnableVertexAttribArray(0);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+    }
+
+    glfwTerminate();
+    return 0;
 }
 
-void processInput(GLFWwindow *window) {
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-    glfwSetWindowShouldClose(window, true);
-  }
+void processInput(GLFWwindow *window){
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
 }
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
-  glViewport(0, 0, width, height);
+void framebuffer_size_callback(GLFWwindow *window, int width, int height){
+    glViewport(0, 0, width, height);
 }
--- shaders/src/shaders/shader.fs
+++ shaders/src/shaders/shader.fs
@@ -1,9 +1,14 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 ourColor;
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
 
-void main()
-{
-    FragColor = vec4(ourColor, 1.0f);
+void main() {
+    vec2 st = gl_FragCoord.xy / u_resolution.xy;
+    st.x *= u_resolution.x / u_resolution.y;
+
+    vec3 color = vec3(st.x, st.y, abs(sin(u_time)));
+    FragColor = vec4(color, 1.0);
 }
