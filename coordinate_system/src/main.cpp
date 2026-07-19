fd98f621e6c65c526004607d8d1e842aac94b66f
Omar Garcia <gomar0428@gmail.com>
2026-07-18 20:48:01 -0700

    Added mulitple spinning cubes to the world

--- coordinate_system/src/main.cpp
+++ coordinate_system/src/main.cpp
@@ -1,7 +1,9 @@
 #include "include/glad/glad.h"
 #include "include/GLFW/glfw3.h"
 #include "include/glm/ext/matrix_clip_space.hpp"
+#include "include/glm/ext/matrix_float4x4.hpp"
 #include "include/glm/ext/matrix_transform.hpp"
+#include "include/glm/ext/vector_float3.hpp"
 #include "include/glm/glm.hpp"
 #include "include/glm/gtc/matrix_transform.hpp"
 #include "include/glm/gtc/type_ptr.hpp"
@@ -100,6 +102,19 @@ int main() {
          0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
         -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
     };
 
     unsigned int VAO, VBO;
@@ -201,27 +216,28 @@ int main() {
         Shader.use();
 
         //create transformations
-        glm::mat4 model         = glm::mat4(1.0f);
         glm::mat4 view          = glm::mat4(1.0f);
         glm::mat4 projection    = glm::mat4(1.0f);
-        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
-        view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
         projection = glm::perspective(glm::radians(45.0f), (float) SCR_WIDTH / (float) SCR_HEIGHT, 0.1f, 100.0f);
-
-        //retrieve the matrix uniform locations
-        unsigned int modelLoc = glGetUniformLocation(Shader.ID, "model");
-        unsigned int viewLoc = glGetUniformLocation(Shader.ID, "view");
-
-        //pass them to the shader (3 dif ways)
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+        view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
         //perspective usually doesnt change so it is more effecient to set it outside the main loop
         Shader.setMat4("projection", projection);
+        Shader.setMat4("view", view);
 
         //render constainer
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        for(unsigned int i{0}; i < 10; i++){
+            //calculate the model matrix for each object and pass it to the shader
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(1.0f, 0.3f, 0.5f));
+            Shader.setMat4("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         glfwSwapBuffers(window);
         glfwPollEvents();
