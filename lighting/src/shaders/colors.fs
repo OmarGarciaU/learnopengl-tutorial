b4268b85fe3642f35e893614fb1a1e8a26c51bdd
Omar Garcia <gomar0428@gmail.com>
2026-07-27 03:33:42 -0700

    Added diffuse lighting to main cube

--- lighting/src/shaders/colors.fs
+++ lighting/src/shaders/colors.fs
@@ -15,11 +15,12 @@ void main()
     vec3 ambient = ambientStrength * lightColor;
 
     //diffuse
-    // vec3 norm = normalize(Normal);
-    // vec3 lightDir = normalize(lightPos - FragPos);
-    // float diff = max(dot(norm, lightDir), 0.0);
-    // vec3 diffuse = diff * lightColor;
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
 
-    vec3 result = (ambient) * objectColor;
+    // no ambient test
+    vec3 result = (ambient + diffuse) * objectColor;
     FragColor = vec4(result , 1.0);
 }
