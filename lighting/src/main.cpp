71b4dfd1747191f284fdaf385c1c74ce72d65cae
Omar Garcia <gomar0428@gmail.com>
2026-07-29 16:04:44 -0700

    Added soft edge to the spotlight

--- lighting/src/main.cpp
+++ lighting/src/main.cpp
@@ -35,7 +35,7 @@ bool firstMouse = true;
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
-glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+//glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
 int main() {
     glfwInit();
@@ -206,6 +206,7 @@ int main() {
         lightingShader.setVec3("light.position", camera.Position);
         lightingShader.setVec3("light.direction", camera.Front);
         lightingShader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
+        lightingShader.setFloat("light.outerCutOff", glm::cos(glm::radians(17.5f)));
         lightingShader.setVec3("viewPos", camera.Position);
 
         //light properties
