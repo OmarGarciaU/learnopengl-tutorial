790274cd4b3c86c3dd4495ba1dabf0d9c33049bd
Omar Garcia <gomar0428@gmail.com>
2026-07-29 15:38:30 -0700

    Added spotlight effect

--- lighting/src/main.cpp
+++ lighting/src/main.cpp
@@ -35,7 +35,7 @@ bool firstMouse = true;
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
-//glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
 int main() {
     glfwInit();
@@ -198,18 +198,23 @@ int main() {
         processInput(window);
 
         //render
-        glClearColor(0.0f, 0.0f, 0.4f, 1.0f);
+        glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         //be sure to activate shader when setting uniforms/drawing objects
         lightingShader.use();
-        lightingShader.setVec3("light.direction", -0.2f, -1.0f, -0.3f);
+        lightingShader.setVec3("light.position", camera.Position);
+        lightingShader.setVec3("light.direction", camera.Front);
+        lightingShader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
         lightingShader.setVec3("viewPos", camera.Position);
 
         //light properties
-        lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
-        lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
+        lightingShader.setVec3("light.ambient", 0.1f, 0.1f, 0.1f);
+        lightingShader.setVec3("light.diffuse", 0.8f, 0.8f, 0.8f);
         lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+        lightingShader.setFloat("light.constant", 1.0f);
+        lightingShader.setFloat("light.linear", 0.09f);
+        lightingShader.setFloat("light.quadratic", 0.032f);
 
         //material properties
         lightingShader.setFloat("material.shininess", 32.0f);
@@ -230,10 +235,6 @@ int main() {
         //bind specular map
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, specularMap);
-        //bind emissionMap
-        //render cube
-        // glBindVertexArray(cubeVAO);
-        // glDrawArrays(GL_TRIANGLES, 0, 36);
 
         glBindVertexArray(cubeVAO);
         for(unsigned int i = 0; i < 10; i++){
@@ -252,12 +253,12 @@ int main() {
         // lightCubeShader.setMat4("view", view);
         // model = glm::mat4(1.0f);
         // model = glm::translate(model, lightPos);
-        // model = glm::scale(model, glm::vec3(0.1f));// a smaller cube
+        // model = glm::scale(model, glm::vec3(0.2f));// a smaller cube
         // lightCubeShader.setMat4("model", model);
-        //
-        // //draw second light
-        // glBindVertexArray(lightCubeVAO);
-        // glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        //draw second light
+        glBindVertexArray(lightCubeVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
