00a0cc9620f9925680b6c6ca2c9e7a90641c914a
Omar Garcia <gomar0428@gmail.com>
2026-07-31 11:33:05 -0700

    Added model loading using assiump lib

---
+++ model_loading copy/src/main.cpp
@@ -0,0 +1,178 @@
+#include "filesystem.h"
+#include "include/glad/glad.h"
+#include "include/GLFW/glfw3.h"
+
+#include "include/glm/ext/matrix_clip_space.hpp"
+#include "include/glm/ext/matrix_float4x4.hpp"
+#include "include/glm/ext/matrix_transform.hpp"
+#include "include/glm/ext/vector_float3.hpp"
+#include "include/glm/trigonometric.hpp"
+
+#include "shaders/shader.h"
+#include "camera.h"
+#include "model.h"
+
+#include <ctime>
+#include <iostream>
+#include <vector>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+using namespace std;
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow *window);
+
+const unsigned int SCR_WIDTH = 1600;
+const unsigned int SCR_HEIGHT = 1200;
+
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+//timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+//glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
+int main() {
+
+    //initialize glfw
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    //glfw window creation
+    //--------------------
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL", NULL, NULL);
+    if(window == NULL){
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    //tell GLFW to capture our mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    //glad: load all openGL function pointers
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        cout << "Failed to initialize GLAD" << endl;
+        return -1;
+    }
+
+    //tell stbi to flip loaded textures on the y-axis before loading model
+    stbi_set_flip_vertically_on_load(true);
+
+    //Configure global opengl state
+    glEnable(GL_DEPTH_TEST);
+
+    // congigure shaders file path
+    const char vertPath[] = "/Users/omargarcia/Desktop/Programming/Repos/Learning_and_Books/learnopengl-tutorial/model_loading/src/shaders/model_loading.vs";
+    const char fragPath[] = "/Users/omargarcia/Desktop/Programming/Repos/Learning_and_Books/learnopengl-tutorial/model_loading/src/shaders/model_loading.fs";
+
+    //initialize shader
+    Shader ourShader(vertPath, fragPath);
+
+    //model path
+    // const char objPath[] = "/Users/omargarcia/Desktop/Programming/Repos/Learning_and_Books/learnopengl-tutorial/model_loading/src/resources/backpack.obj";
+
+    //load models
+    Model ourModel("/Users/omargarcia/Desktop/Programming/Repos/Learning_and_Books/learnopengl-tutorial/model_loading/src/resources/backpack.obj");
+    cout << "meshes loaded: " << ourModel.meshes.size() << endl;
+
+    //Random color generator
+    srand(time(0));
+    std::vector<float> fps = {};
+
+    while(!glfwWindowShouldClose(window)){
+
+        //per-frame time logic
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        //cout << 1.0f / deltaTime << endl;
+
+        //input
+        processInput(window);
+
+        //render
+        glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        ourShader.use();
+
+        //be sure to activate shader when setting uniforms/drawing objects
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view = camera.GetViewMatrix();
+        ourShader.setMat4("projection", projection);
+        ourShader.setMat4("view", view);
+
+        //render loaded model
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(0.3f));
+        ourShader.setMat4("model", model);
+        ourModel.Draw(ourShader);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+void processInput(GLFWwindow *window){
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
+    float cameraSpeed = static_cast<float>(2.5 * deltaTime);
+    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) camera.ProcessKeyboard(FORWARD, deltaTime);
+    if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) camera.ProcessKeyboard(LEFT, deltaTime);
+    if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) camera.ProcessKeyboard(RIGHT, deltaTime);
+    if(glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) camera.ProcessKeyboard(UP, deltaTime);
+    if(glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS) camera.ProcessKeyboard(DOWN, deltaTime);
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height){
+    glViewport(0, 0, width, height);
+}
+
+//glfw: Whenever the mouse moves, thios callback is called
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn){
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);;
+
+    if(firstMouse){
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.processMouseMovement(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset){
+    camera.processMouseScroll(static_cast<float>(yoffset));
+}
